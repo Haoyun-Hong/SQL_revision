7b4c0c4276afddc470ec0f5681115cd70d446a7f
Haoyun-Hong <haoyunhong@berkeley.edu>
2023-01-05 20:26:18 -0800

    finalized notes on baisc SQL queries

--- basicSQL.sql
+++ basicSQL.sql
@@ -42,6 +42,54 @@ FROM orders
 ORDER BY account_id, total_amt_usd DESC; 
 
 /*
-WHERE
-
+WHERE: filter a set of result based on specific critera
+goes after FROM but before ORDER BY
+Comparison operators: 
+Note: equals is only =, not "=="
+Note: use single quotes '', not double quotes ""
 */
+
+SELECT *
+FROM orders
+WHERE gloss_amt_usd >= 1000
+LIMIT 5; 
+
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil'; 
+
+/*
+Derived Column and Alias
+keep the alias name lower case and without blank space
+*/
+
+SELECT id, account_id, standard_amt_usd / standard_qty AS unit_price 
+FROM orders
+LIMIT 10;
+
+/*
+Logical Operations:
+LIKE: wild card matching 
+eg: '%google%' start and end with any number of characters
+IN: filter data based on several possble values 
+BETWEEN:a cleaner statement than AND, is inclusive for numerical valus 
+For date values, the asummed time is at 00:00:00
+eg: to include all dates from 2016, we can do BETWEEN '2016-01-01' AND '2017-01-01'
+*/ 
+
+SELECT name 
+FROM accounts
+WHERE name LIKE '%google%';
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name in ('Walmart', 'Target', 'Nordstrom'); 
+
+WHERE column >= 6 AND column <= 10
+
+# This is equivalent to 
+
+WHERE column BETWEEN 6 AND 10
+
+
+
